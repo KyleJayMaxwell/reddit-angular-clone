fece093ad31f8a9275aa22d008660aa704ce90a2
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-04-05 21:41:01 -0600

    inserted test data

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,14 @@
-// sample angular code
 
 var app = angular.module('myApp', []);
 
 app.controller('myController', ['$scope', function($scope) {
   $scope.greeting = "Hello World!";
+
+  $scope.posts = [
+    {
+      title: 'Berlin',
+      author: 'Bert Anders',
+    }
+  ];
+
 }]);
