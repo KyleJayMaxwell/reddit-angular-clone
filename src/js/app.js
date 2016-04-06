a91b3be205e3caea885780b67bcced7169c4bf03
Kyle Maxwell <kylejaymaxwell@gmail.com>
2016-04-06 10:01:48 -0600

    made more data

--- src/js/app.js
+++ src/js/app.js
@@ -15,7 +15,7 @@ app.controller('mainController', ['$scope', function($scope) {
       author: 'Bert Anders',
       date: '04/01/2016',
       votes: 8,
-      image: 'https://placebear.com/300/300',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Siegessaeule_Aussicht_10-13_img4_Tiergarten.jpg/1920px-Siegessaeule_Aussicht_10-13_img4_Tiergarten.jpg',
       description: 'Berlin is a bon vivant, passionately feasting on the smorgasbord of life, never taking things – or itself – too seriously. To me, this city is nothing short of addictive. It embraces me, inspires me, accepts me and makes me feel good about myself, the world and other people. I enjoy its iconic sights, its vast swaths of green, its sky bars and chic restaurants, but I love its gritty sides more. There\'s nothing static about Berlin: it\'s unpredictable, unpretentious and irresistible. And it loves you back – if you let it in.',
       comments: [
         { author: 'andy',
@@ -29,7 +29,7 @@ app.controller('mainController', ['$scope', function($scope) {
       author: 'Harry Hawkins',
       date: '04/05/2016',
       votes: 4,
-      image: 'https://placebear.com/300/300',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/8/83/Hamburg_montage.jpg',
       description: 'In Hamburg I loved A strange girl, She put my whole being In a whirl, She spurned everybody But me, I made her happy, In Hamburg. But if she had Spurned me, I\'d have looked her in the eye, And run away, And in my room, I would have cried, I might even have died, In Hamburg.',
       comments: [
         { author: 'haden12',
@@ -38,6 +38,34 @@ app.controller('mainController', ['$scope', function($scope) {
           comment: 'HAMBURG!'},
       ]
     },
+    {
+      title: 'Munich',
+      author: 'Maxwell Atkin',
+      date: '04/06/2016',
+      votes: 2,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Munchen_collage.jpg/600px-Munchen_collage.jpg',
+      description: 'The name of the city is derived from the Old/Middle High German term Munichen, meaning "by the monks". It derives from the monks of the Benedictine order who ran a monastery at the place that was later to become the Old Town of Munich; hence the monk depicted on the city\'s coat of arms. Munich was first mentioned in 1158. From 1255 the city was seat of the Bavarian Dukes. Black and gold—the colours of the Holy Roman Empire—have been the city\'s official colours since the time of Ludwig the Bavarian, when it was an imperial residence. Following a final reunification of the Wittelsbachian Duchy of Bavaria, previously divided and sub-divided for more than 200 years, the town became the country\'s sole capital in 1506.',
+      comments: [
+        { author: 'FCB',
+          comment: 'BAYERN!'},
+        { author: 'Johan',
+          comment: 'Munich!'},
+      ]
+    },
+    {
+      title: 'Frankfurt',
+      author: 'Frank Antony',
+      date: '04/04/2016',
+      votes: 0,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/FrankfurtSkyline2014.jpg/560px-FrankfurtSkyline2014.jpg',
+      description: 'Frankfurt is a centre for commerce, culture, education, tourism and web traffic. Messe Frankfurt is one of the world\'s largest trade fairs at 578,000 square metres and ten exhibition halls. Major trade fairs include the Frankfurt Motor Show, the world\'s largest motor show, and the Frankfurt Book Fair, the world\'s largest book fair.',
+      comments: [
+        { author: 'Franky1',
+          comment: 'FRANK furt!'},
+        { author: 'Rob',
+          comment: 'Frankfurt!'},
+      ]
+    }
   ];
 
 }]);
